a623a71c6c7311cdd39acb4a4de9d813b467bb8a
ebruturk <ebruturk1443@gmail.com>
2024-06-07 12:24:41 +0300

    first commit

---
+++ src/test/java/clarusway/homework/Homework30.java
@@ -0,0 +1,12 @@
+package clarusway.homework;
+
+
+public class Homework30 {
+    /*
+    Given
+        Go to https://www.hepsiburada.com/bilgisayarlar-c-2147483646
+    When
+        Click all marks and print their names
+        (Soldaki markaların hepsine tıklayıp adlarını yazdırınız)
+*/
+}
